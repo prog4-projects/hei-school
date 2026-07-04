f825210aadcf3436b5274831de6b0655a346577e
Njaka <hei.herinjaka@gmail.com>
2026-07-04 20:38:49 +0300

    fix: correct JUser entity mapping and constraints

--- src/main/java/com/hei/school/repository/model/JUser.java
+++ src/main/java/com/hei/school/repository/model/JUser.java
@@ -2,23 +2,33 @@ package com.hei.school.repository.model;
 
 import jakarta.persistence.*;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
 @Entity
-@Table(name = "User")
-@AllArgsConstructor
+@Table(name = "users")
+@Getter
+@Setter
 @NoArgsConstructor
-@Data
+@AllArgsConstructor
 @Builder
 public class JUser {
-  @Id @GeneratedValue @UuidGenerator private UUID id;
 
-  @Column private String firstName;
-  @Column private String lastName;
-  @Column private String userName;
-  @Column private String email;
+  @Id
+  @GeneratedValue
+  @UuidGenerator
+  private UUID id;
+
+  @Column(nullable = false)
+  private String firstName;
+
+  @Column(nullable = false)
+  private String lastName;
+
+  @Column(nullable = false, unique = true)
+  private String userName;
+
+  @Column(nullable = false, unique = true)
+  private String email;
 }
