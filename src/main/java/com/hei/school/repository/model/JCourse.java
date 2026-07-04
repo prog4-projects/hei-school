8298cddb08c157067da879d687438eb089e95076
Njaka <hei.herinjaka@gmail.com>
2026-07-04 20:38:12 +0300

    fix: correct JCourse entity mapping and constraints

--- src/main/java/com/hei/school/repository/model/JCourse.java
+++ src/main/java/com/hei/school/repository/model/JCourse.java
@@ -5,28 +5,34 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 import org.hibernate.annotations.Check;
 import org.hibernate.annotations.UuidGenerator;
 
 @Entity
-@Table(name = "Course")
-@AllArgsConstructor
+@Table(name = "courses")
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
-@Data
 @Check(constraints = "end_date > start_date")
 public class JCourse {
-  @Id @GeneratedValue @UuidGenerator private UUID id;
 
-  @Column private String title;
-  @Column private Instant startDate;
-
-  @Column private Instant endDate;
+  @Id
+  @GeneratedValue
+  @UuidGenerator
+  private UUID id;
 
   @ManyToMany(mappedBy = "courses")
   private Set<JUser> users = new HashSet<>();
+  @Column(nullable = false)
+  private String title;
+
+  @Column(name = "start_date", nullable = false)
+  private Instant startDate;
+
+  @Column(name = "end_date", nullable = false)
+  private Instant endDate;
 }
