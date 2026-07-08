acc9f48dc7993ebbd681320e8134498249b8717b
Njaka <hei.herinjaka@gmail.com>
2026-07-08 14:10:19 +0300

    refactor(mapper): add mappers for User, Course and Enrollment entities

---
+++ src/main/java/com/hei/school/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.hei.school.mapper;
+
+import com.hei.school.entity.User;
+import com.hei.school.repository.model.JUser;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+
+    public User toDomain(JUser jUser){
+
+        return User.builder()
+                .id(jUser.getId())
+                .firstName(jUser.getFirstName())
+                .lastName(jUser.getLastName())
+                .userName(jUser.getUserName())
+                .email(jUser.getEmail())
+                .build();
+
+    }
+
+}
