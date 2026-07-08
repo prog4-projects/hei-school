0ec80d7c4bd3968910eb0b2cc6c4041404305ba5
anthonyRanivoarison <antonitsiory@gmail.com>
2026-07-08 13:15:50 +0300

    feat: implement event-driven email notification for course enrollment

---
+++ src/main/java/com/hei/school/endpoint/event/model/SendEmailRequested.java
@@ -0,0 +1,31 @@
+package com.hei.school.endpoint.event.model;
+
+import java.time.Duration;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class SendEmailRequested extends PojaEvent {
+  private String to;
+  private String subject;
+  private String htmlBody;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(45);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(60);
+  }
+}
--- src/main/java/com/hei/school/endpoint/rest/controller/EnrollmentController.java
+++ src/main/java/com/hei/school/endpoint/rest/controller/EnrollmentController.java
@@ -4,7 +4,10 @@ import com.hei.school.service.EnrollmentService;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
